5ac9ddc92da8ef48adb92cc69dc245b25f331940
jing-liao <jing.liao-ns@scopely.com>
2023-02-10 12:22:33 +0100

    add logic for 1st rule

--- Assets/Scripts/Tests/Editor/GameOfLife.cs
+++ Assets/Scripts/Tests/Editor/GameOfLife.cs
@@ -11,23 +11,86 @@
         
         public bool[,] AdvanceToNextGeneration()
         {
-            int neighbourCounter;
+            int neighbourCounter = 0;
             
             for (int i = 0; i < _currentState.GetLength(0); i++)
             {
                 for (int j = 0; j < _currentState.GetLength(1); j++)
                 {
-                    if (_currentState[i, j])
+                    if (!_currentState[i, j]) continue;
+
+                    if (CheckNeighbourCellIsAlive(i - 1, j - 1))
                     {
-                        if (_currentState[i - 1, j - 1])
-                        {
-                            
-                        }
-                            
+                        neighbourCounter++;
+                    }
+
+                    if (CheckNeighbourCellIsAlive(i - 1, j))
+                    {
+                        neighbourCounter++;
+                    }
+
+                    if (CheckNeighbourCellIsAlive(i - 1, j + 1))
+                    {
+                        neighbourCounter++;
+                    }
+
+                    if (CheckNeighbourCellIsAlive(i, j - 1))
+                    {
+                        neighbourCounter++;
+                    }
+
+                    if (CheckNeighbourCellIsAlive(i, j))
+                    {
+                        neighbourCounter++;
+                    }
+
+                    if (CheckNeighbourCellIsAlive(i, j + 1))
+                    {
+                        neighbourCounter++;
+                    }
+
+                    if (CheckNeighbourCellIsAlive(i + 1, j - 1))
+                    {
+                        neighbourCounter++;
+                    }
+
+                    if (CheckNeighbourCellIsAlive(i + 1, j))
+                    {
+                        neighbourCounter++;
+                    }
+
+                    if (CheckNeighbourCellIsAlive(i + 1, j + 1))
+                    {
+                        neighbourCounter++;
+                    }
+
+                    if (neighbourCounter < 2)
+                    {
+                        _currentState[i, j] = false;
                     }
                 }
             }
             return _currentState;
+        }
+
+        private int GetNumberOfNeighboursAlive(int i, int j)
+        {
+            for (int x = -1; x < 2; x++)
+            {
+                
+            }
+
+            return 0;
+        }
+
+        private bool CheckNeighbourCellIsAlive(int i, int j)
+        {
+            return !IsIndexOutOfBoundary(i, j) && _currentState[i, j];
+        }
+
+        private bool IsIndexOutOfBoundary(int i, int j)
+        {
+            return i < 0 || i >= _currentState.GetLength(0) || j < 0 || j >= _currentState.GetLength(1);
         }
     }
 }
